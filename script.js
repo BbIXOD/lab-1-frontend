80e4f4ea62fb71e0655d95eb4e79cbd01124e43b
Maksym Diachok <maksdyachok2005@gmail.com>
2024-10-15 21:32:05 +0300

    Add functions to work with image in js.

--- script.js
+++ script.js
@@ -3,17 +3,38 @@
 const firstStyle = 'modified1'
 const secondStyle = 'modified2'
 
+const image = document.getElementById('city-image')
+const imageContainer = document.getElementById('city-image-container')
+let width = image.clientWidth
+let imagePresent = true
+
 const changeStyleById = () => {
     const el = document.getElementById('element4')
-    changeStyle(el)
+    _changeStyle(el)
 }
 
 const getElementByQuery = () => {
     const el = document.querySelector('li.query-find')
-    changeStyle(el)
+    _changeStyle(el)
 }
 
-const changeStyle = (el) => {
+const scaleImage = (scaleChange) => {
+    if (!imagePresent) return
+    width += scaleChange
+    image.style.width = width + "px"
+}
+const deleteImage = () => {
+    if (!imagePresent) return
+    image.remove()
+    imagePresent = false
+}
+const addImage = () => {
+    if (imagePresent) return;
+    imageContainer.appendChild(image)
+    imagePresent = true
+}
+
+const _changeStyle = (el) => {
     const classList = el.classList
     const toFirstStyle = classList.contains(secondStyle)
 
